b2ccb2f9ac9e59ce68c95e8392501183ecffaab0
Frank van der Stam <frank_stam5@hotmail.com>
2023-04-08 16:33:33 +0200

    Fixed VirtualMemoryFlag for ER 1.09

--- soulmemory-rs/src/games/elden_ring.rs
+++ soulmemory-rs/src/games/elden_ring.rs
@@ -32,7 +32,7 @@ pub struct EldenRing
     process: Process,
 
     event_flags: Arc<Mutex<Vec<EventFlag>>>,
-    event_flag_man: Pointer,
+    virtual_memory_flag: Pointer,
     fn_get_event_flag: FnGetEventFlag,
 }
 
@@ -45,7 +45,7 @@ impl EldenRing
             process: Process::new("eldenring.exe"),
 
             event_flags: Arc::new(Mutex::new(Vec::new())),
-            event_flag_man: Pointer::default(),
+            virtual_memory_flag: Pointer::default(),
             fn_get_event_flag: |_,_|{0},
         }
     }
@@ -60,7 +60,7 @@ impl EventFlagLogger for EldenRing
     }
 
     fn get_event_flag_state(&self, event_flag: u32) -> bool {
-        let result = (self.fn_get_event_flag)(self.event_flag_man.read_u64_rel(None), event_flag);
+        let result = (self.fn_get_event_flag)(self.virtual_memory_flag.read_u64_rel(None), event_flag);
         return result == 1;
     }
 }
@@ -74,7 +74,7 @@ impl Game for EldenRing
             {
                 self.process.refresh()?;
 
-                self.event_flag_man = self.process.scan_rel("SprjEventFlagMan", "48 83 3d ? ? ? ? 00 75 46 4c 8b 05 ? ? ? ? 4c 89 44 24 40 ba 08 00 00 00 b9 c8 01 00 00", 3, 8, vec![0])?;
+                self.virtual_memory_flag = self.process.scan_rel("VirtualMemoryFlag", "48 8b 3d ? ? ? ? 8b f3 89 5c 24 20 48 85 ff", 3, 7, vec![0])?;
 
                 let set_event_flag_address = self.process.scan_abs("set_event_flag", "48 89 5c 24 08 44 8b 49 1c 44 8b d2 33 d2 41 8b c2 41 f7 f1 41 8b d8 4c 8b d9", 0, Vec::new())?.get_base_address();
                 let get_event_flag_address = self.process.scan_abs("get_event_flag", "44 8b 41 1c 44 8b da 33 d2 41 8b c3 41 f7 f0", 0, Vec::new())?.get_base_address();
@@ -88,7 +88,7 @@ impl Game for EldenRing
                     STATIC_DETOUR_SET_EVENT_FLAG.call(rdx, event_flag_id, value);
                 }).unwrap().enable().unwrap();
 
-                info!("event_flag_man base address: 0x{:x}", self.event_flag_man.get_base_address());
+                info!("event_flag_man base address: 0x{:x}", self.virtual_memory_flag.get_base_address());
                 info!("set event flag address     : 0x{:x}", set_event_flag_address);
                 info!("get event flag address     : 0x{:x}", get_event_flag_address);
             }

04ea95204d439725971939b84f4b676fb8632b12
Frank van der Stam <frank_stam5@hotmail.com>
2023-09-11 08:53:09 +0200

    Fix event flag man scan for 1.1.2. Check if hook is still installed, rehook if the game kicks it out.

--- soulmemory-rs/src/games/armored_core_6.rs
+++ soulmemory-rs/src/games/armored_core_6.rs
@@ -34,6 +34,7 @@ pub struct ArmoredCore6
     event_flags: Arc<Mutex<Vec<EventFlag>>>,
     virtual_memory_flag: Pointer,
     fn_get_event_flag: FnGetEventFlag,
+    set_event_flag_address: usize,
 }
 
 impl ArmoredCore6
@@ -47,6 +48,7 @@ impl ArmoredCore6
             event_flags: Arc::new(Mutex::new(Vec::new())),
             virtual_memory_flag: Pointer::default(),
             fn_get_event_flag: |_,_|{0},
+            set_event_flag_address: 0,
         }
     }
 }
@@ -70,37 +72,48 @@ impl EventFlagLogger for ArmoredCore6
 
 impl Game for ArmoredCore6
 {
-    fn refresh(&mut self) -> Result<(), String> {
-        if !self.process.is_attached()
+    fn refresh(&mut self) -> Result<(), String>
+    {
+        unsafe
         {
-            unsafe
+            if !self.process.is_attached()
+            {
+                self.process.refresh()?;
+
+                self.virtual_memory_flag = self.process.scan_rel("CSEventFlagMan", "48 8b 35 ? ? ? ? 83 f8 ff 0f 44 c1", 3, 7, vec![0])?;
+
+                self.set_event_flag_address = self.process.scan_abs("set_event_flag", "48 89 5c 24 18 56 41 56 41 57 48 83 ec 20 44 8b 49 1c 44 8b f2", 0, Vec::new())?.get_base_address();
+                let get_event_flag_address = self.process.scan_abs("get_event_flag", "44 8b 41 1c 44 8b da 33 d2 41 8b c3 41 f7 f0 4c 8b d1 45 33 c9 44 0f af c0", 0, Vec::new())?.get_base_address();
+                self.fn_get_event_flag = mem::transmute(get_event_flag_address);
+
+                let event_flags = Arc::clone(&self.event_flags);
+                STATIC_DETOUR_SET_EVENT_FLAG.initialize(mem::transmute(self.set_event_flag_address), move |rdx: u64, event_flag_id: u32, value: i32|
                 {
-                    self.process.refresh()?;
+                    let mut guard = event_flags.lock().unwrap();
+                    guard.push((chrono::offset::Local::now(), event_flag_id, value == 1));
+                    STATIC_DETOUR_SET_EVENT_FLAG.call(rdx, event_flag_id, value);
+                }).unwrap().enable().unwrap();
 
-                    self.virtual_memory_flag = self.process.scan_rel("CSEventFlagMan", "48 8b 35 20 4a df 03 83 f8 ff 0f 44 c1", 3, 7, vec![0])?;
-
-                    let set_event_flag_address = self.process.scan_abs("set_event_flag", "48 89 5c 24 18 56 41 56 41 57 48 83 ec 20 44 8b 49 1c 44 8b f2", 0, Vec::new())?.get_base_address();
-                    let get_event_flag_address = self.process.scan_abs("get_event_flag", "44 8b 41 1c 44 8b da 33 d2 41 8b c3 41 f7 f0 4c 8b d1 45 33 c9 44 0f af c0", 0, Vec::new())?.get_base_address();
-                    self.fn_get_event_flag = mem::transmute(get_event_flag_address);
-
-                    let event_flags = Arc::clone(&self.event_flags);
-                    STATIC_DETOUR_SET_EVENT_FLAG.initialize(mem::transmute(set_event_flag_address), move |rdx: u64, event_flag_id: u32, value: i32|
-                    {
-                        let mut guard = event_flags.lock().unwrap();
-                        guard.push((chrono::offset::Local::now(), event_flag_id, value == 1));
-                        STATIC_DETOUR_SET_EVENT_FLAG.call(rdx, event_flag_id, value);
-                    }).unwrap().enable().unwrap();
-
-                    info!("event_flag_man base address: 0x{:x}", self.virtual_memory_flag.get_base_address());
-                    info!("set event flag address     : 0x{:x}", set_event_flag_address);
-                    info!("get event flag address     : 0x{:x}", get_event_flag_address);
+                info!("event_flag_man base address: 0x{:x}", self.virtual_memory_flag.get_base_address());
+                info!("set event flag address     : 0x{:x}", self.set_event_flag_address);
+                info!("get event flag address     : 0x{:x}", get_event_flag_address);
+            }
+            else
+            {
+                let mut buffer: [u8; 1] = [0x0];
+                self.process.read_memory_abs(self.set_event_flag_address, &mut buffer);
+                let byte = buffer[0];
+                if byte == 0x48
+                {
+                    info!("re-hook set event flag");
+                    STATIC_DETOUR_SET_EVENT_FLAG.disable().unwrap();
+                    STATIC_DETOUR_SET_EVENT_FLAG.enable().unwrap();
                 }
+
+                self.process.refresh()?;
+            }
+            Ok(())
         }
-        else
-        {
-            self.process.refresh()?;
-        }
-        Ok(())
     }
 
     fn get_dx_version(&self) -> DxVersion {
